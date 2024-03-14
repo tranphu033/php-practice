56c9fa66ada98206f3689fb9ed87a994a200dce8
tranphu <tranphudhak@gmail.com>
2024-03-14 15:38:37 +0700

    16-array_more

--- 16-array.php
+++ 16-array.php
@@ -12,3 +12,30 @@ $car["year"] = 1964;
 foreach ($car as $x => $y) {
     echo "$x: $y <br>";
 }
+
+$cars = array("Volvo", "BMW", "Toyota");
+array_splice($cars, 1, 1);
+unset($cars[0]);
+print_r($cars);
+echo "<br/>";
+
+$cars = array("brand" => "Ford", "model" => "Mustang", "year" => 1964);
+unset($cars["model"]);
+print_r($cars);
+echo "<br/>";
+
+$cars = array("brand" => "Ford", "model" => "Mustang", "year" => 1964);
+$newarray = array_diff($cars, ["Mustang", 1964]);
+print_r($newarray);
+echo "<br/>";
+
+$cars = array("Volvo", "BMW", "Toyota");
+sort($cars);
+print_r($cars);
+echo "<br/>";
+
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+asort($age);
+print_r($age);
+echo "<br/>";
+
