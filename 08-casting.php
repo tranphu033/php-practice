08dbe758995ce5ababbdf142db84c52eaf39c5ef
tranphu <tranphudhak@gmail.com>
2024-03-14 14:31:20 +0700

    08-casting

---
+++ 08-casting.php
@@ -0,0 +1,79 @@
+<?php
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (string) $a;
+$b = (string) $b;
+$c = (string) $c;
+$d = (string) $d;
+$e = (string) $e;
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "25 kilometers"; // String
+$d = "kilometers 25"; // String
+$e = "hello"; // String
+$f = true;    // Boolean
+$g = NULL;    // NULL
+
+$a = (int) $a;
+$b = (int) $b;
+$c = (int) $c;
+$d = (int) $d;
+$e = (int) $e;
+$f = (int) $f;
+$g = (int) $g;
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (array) $a;
+$b = (array) $b;
+$c = (array) $c;
+$d = (array) $d;
+$e = (array) $e;
+
+class Car
+{
+    public $color;
+    public $model;
+    public function __construct($color, $model)
+    {
+        $this->color = $color;
+        $this->model = $model;
+    }
+    public function message()
+    {
+        return "My car is a " . $this->color . " " . $this->model . "!";
+    }
+}
+
+$myCar = new Car("red", "Volvo");
+
+$myCar = (array) $myCar;
+var_dump($myCar);
+echo "<br/>";
+
+$a = 5;       // Integer
+$b = 5.34;    // Float
+$c = "hello"; // String
+$d = true;    // Boolean
+$e = NULL;    // NULL
+
+$a = (object) $a;
+$b = (object) $b;
+$c = (object) $c;
+$d = (object) $d;
+$e = (object) $e;
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+var_dump($d);
+var_dump($e);
